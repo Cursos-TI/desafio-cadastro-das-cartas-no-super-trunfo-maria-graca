8d9f9f4273827cb312c256a4616cfdce8d654b93
maria-graca <gravit.santos@hotmail.com>
2025-03-18 00:40:47 +0000

    printf("\Desafio Super Trunfo Maria Graca Novato\n")

--- maria graca novato.c
+++ maria graca novato.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+int main(){
+    printf("\Desafio Super Trunfo Maria Graca Novato\n");
 
+        int carta1;
+        int pontosturisticos1;
+        int populacao1;
+        char estado1 [1] ;
+        char cidade1[10];
+        char codigo1 [3];
+        float areadacidade1;
+
+      
+        printf("Digite a carta \n");
+        scanf("%d", &carta1);
+     
+        printf("Digite  os pontos turisticos:  \n");
+        scanf("%d", &pontosturisticos1);
+     
+        printf("Digite a populacao: \n");
+        scanf("%d", &populacao1);
+     
+        printf("Digite o estado: \n");
+        scanf("%c", &estado1);
+
+        printf("Digite a cidade: \n");
+        scanf("%s", &cidade1);
+
+        printf("Digite o codigo da conta: \n");
+        scanf("%s", &codigo1);
+
+        printf("Digite area da cidade1: \n");
+        scanf("%f", &areadacidade1);
+
+        printf("a carta é: %d - Pontos turisticos1: %d- populacao1 de: %f - o estado1 é: %c - a cidade1: %s o codigo1: %s - areadacidade1: %f \n", carta1, pontosturisticos1, populacao1,estado1, cidade1,codigo1, areadacidade1);
+         scan("carta: %d - pontos turisticos: %d - populacao: %d \n", carta1, pontosturisticos1, populacao1,estado1, cidade1,codigo1, areadacidade1);
+         int carta2; 
+         int pontosturisticos2;
+         int populacao2;
+         char estado2[1];
+         char cidade2[10];
+         char codigo2 [3];
+         float areadacidade2;
+
+     printf("Digite a carta2: \n");
+      scanf("%d", &carta2);
+
+     printf("Digite  os pontos turisticos:  \n");
+   scanf("%d", &pontosturisticos2);
+
+   printf("Digite a populacao: \n");
+   scanf("%d", &populacao2);
+
+   printf("Digite o estado: \n");
+   scanf("%c", &estado2);
+
+   printf("Digite a cidade: \n");
+   scanf("%s", &cidade2);
+
+   printf("Digite o codigo: \n");
+   scanf("%s", &codigo2);
+
+   printf("Digite area da cidade: \n");
+   scanf("%f", &areadacidade2);
+
+   printf("a carta é: %d - Pontos turisticos2: %d- populacao2 de: %f - o estado2 é: %c - a cidade2: %s o codigo2: %s - areadacidade2: %f \n", carta2, pontosturisticos2, populacao2,estado2, cidade2,codigo2, areadacidade2);
+scanf("carta: %d - pontos turisticos: %d - populacao: %d \n", carta2, pontosturisticos2, populacao2,estado2, cidade2,codigo2, areadacidade2);
+
+
+}
